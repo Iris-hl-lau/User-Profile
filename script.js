6d145fe2867dd7bae0c014611f450a9c1f40cce4
vyphan009 <vyphan.cnp@gmail.com>
2019-03-25 01:59:30 -0700

    add img

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function uploadImage(){
-    var preview = document.querySelector('img'); //selects the query named img
-    var file    = document.querySelector('input[type=file]').files[0]; //sames as here
-    var reader  = new FileReader();
+    var preview = document.querySelector('img'); 
+       var file    = document.querySelector('input[type=file]').files[0]; 
+       var reader  = new FileReader();
 
-    reader.onloadend = function () {
-        preview.src = reader.result;
-    }
+       reader.onloadend = function () {
+           preview.src = reader.result;
+       }
 
-    if (file) {
-        reader.readAsDataURL(file); 
-    } else {
-        preview.src = "./images/empty-avatar.jpg";
-    }
+       if (file) {
+           reader.readAsDataURL(file); 
+       } else {
+           preview.src = "./images/empty-avatar.jpg";
+       }
 
 }
 
@@ -19,3 +19,4 @@ function edit(id){
     var id = document.getElementById(id);
     id.readOnly = false;
 }
+
